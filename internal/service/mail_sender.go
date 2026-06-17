45eda06d47732a420a32fe0106dd34b2b3e3979d
DANCINGCODES\zhang <dancingcodes@qq.com>
2026-06-17 14:19:41 +0800

    1

--- internal/service/mail_sender.go
+++ internal/service/mail_sender.go
@@ -2,7 +2,7 @@ package service
 
 import "errors"
 
-var ErrMailSenderNotConfigured = errors.New("mail sender not configured")
+var ErrMailSenderNotConfigured = errors.New("邮件发送服务未配置")
 
 type MailSender interface {
 	SendVerificationCode(email string, code string) error
